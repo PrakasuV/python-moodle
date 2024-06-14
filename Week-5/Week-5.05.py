ddb3b49cce12ca7621168d37c8afaa6d61a831db
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 22:57:52 +0530

    Update Week-5.05.py

--- Week-5/Week-5.05.py
+++ Week-5/Week-5.05.py
@@ -1,58 +1,54 @@
-Two string values S1, S2 are passed as the input. The program must print first N characters present in S1 which are also present in S2.
+Consider the below words as key words and check the given input is key word or not.
 
-Input Format:
+keywords: {break, case, continue, default, defer, else, for, func, goto, if, map, range, return, struct, type, var}
 
-The first line contains S1.
-The second line contains S2.
-The third line contains N.
+Input format:
 
-Output Format:
+Take string as an input from stdin.
 
-The first line contains the N characters present in S1 which are also present in S2.
+Output format:
 
-Boundary Conditions:
+Print the word is key word or not.
 
-2 <= N <= 10
-2 <= Length of S1, S2 <= 1000
+Example Input:
 
-Example Input/Output 1:
-
-Input:
-
-abcbde
-cdefghbb
-3
+break
 
 Output:
 
-bcd
+break is a keyword
 
-Note:
+Example Input:
 
-b occurs twice in common but must be printed only once.
+IF
+
+Output:
+
+IF is not a keyword
+
+
+For example:
+
+Input	Result
+break
+break is a keyword
+IF
+IF is not a keyword
 
 
 
 
 
 
-S1 = input().strip()
-S2 = input().strip()
-N = int(input().strip())
-
-common_chars = set(S1) & set(S2)
-
-output = ''
-count = 0
-added_chars = set()  # Set to store added characters
-
-for char in S1:
-    if char in common_chars and char not in added_chars:
-        output += char
-        added_chars.add(char)
-        count += 1
-        if count == N:
-            break
-
-print(output)
+keywords=['break','case','continue','default','defer','else','for','func','goto','if','map','range','return','struct','type','var']
+s=input()
+flag=0
+for i in keywords:
+    if(s==i):
+        flag=1
+        break
+if(flag==1):
+    print(s,'is a keyword')
+else:
+    print(s,'is not a keyword')
 
