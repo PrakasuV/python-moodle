a37a81ef4481c4fe8204b9e0d42224a3515afae0
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 22:31:47 +0530

    Update Week-3.09.py

--- Week-3/Week-3.09.py
+++ Week-3/Week-3.09.py
@@ -1,32 +1,65 @@
-Second last digit
+A triangle can be classified based on the lengths of its sides as equilateral, isosceles or scalene. All three sides of an equilateral triangle have the same length. An isosceles triangle has two sides that are the same length, and a third side that is a different length. If all of the sides have different lengths then the triangle is scalene.
 
-Write a program that returns the second last digit of the given number. Second last digit is being referred 10the digit in the tens place in the given number.
+Write a program that reads the lengths of the three sides of a triangle from the user. Then display a message that states the triangle’s type.
 
-For example, if the given number is 197, the second last digit is 9.
+Sample Input 1
 
-Note1 - The second last digit should be returned as a positive number. i.e. if the given number is -197, the second last digit is 9.
+60
 
-Note2 - If the given number is a single digit number, then the second last digit does not exist. In such cases, the program should return -1. i.e. if the given number is 5, the second last digit should be returned as -1.
+60
+
+60
+
+Sample Output 1
+
+That's a equilateral triangle
+
+Sample Input 2
+
+40
+
+40
+
+80
+
+Sample Output 2
+
+That's a isosceles triangle
+
+Sample Input 3
+
+50
+
+60
+
+70
+
+Sample Output 3
+
+That's a scalene triangle
+
 
 For example:
 
 Input	Result
+60
+60
+60
+That's a equilateral triangle
+40
+40
+80
+That's a isosceles triangle
 
-197	9
 
 
 
 a=int(input())
-
-b=str(abs(a))
-
-l=len(b)
-
-if(l>1):
-
-    print(int(b[-2]))
-
+b=int(input())
+c=int(input())
+if(a==b and b==c):
+    print("That's a equilateral triangle")
+elif(a==b or b==c or c==a):
+    print("That's a isosceles triangle")
 else:
-
-    print(-1)
-
+    print("That's a scalene triangle")
