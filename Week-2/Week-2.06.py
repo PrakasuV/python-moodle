f1a95df29c806772ac13f94158f49d6f4e89f196
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 16:47:56 +0530

    Update Week-2.06.py

--- Week-2/Week-2.06.py
+++ Week-2/Week-2.06.py
@@ -1,40 +1,26 @@
-Eligible to donate blood
+Write a program that returns the last digit of the given number. Last digit is being referred to the least significant digit i.e. the digit in the ones (units) place in the given number.
 
-A team from the Rotract club had planned to conduct a rally to create awareness among the Coimbatore people to donate blood. They conducted the rally successfully. Many of the Coimbatore people realized it and came forward to donate their blood to nearby blood banks. The eligibility criteria for donating blood are people should be above or equal to 18 and his/ her weight should be above 40. There was a huge crowd and staff in the blood bank found it difficult to manage the crowd. So they decided to keep a system and ask the people to enter their age and weight in the system. If a person is eligible he/she will be allowed inside.
+The last digit should be returned as a positive number.
 
- Write a program and feed it to the system to find whether a person is eligible or not.
+For example,
+
+if the given number is 197, the last digit is 7
+
+if the given number is -197, the last digit is 7
 
 
 
-Input Format:
+For example:
 
-Input consists of two integers that correspond to the age and weight of a person respectively.
+Input	Result
+197
+7
+-197
+7
 
-Output Format:
-
-Display True(IF ELIGIBLE)
-
-Display False (if not eligible)
-
-Sample Input
-
-19
-
-45
-
-Sample Output
-
-True
 
 a=int(input())
-
-b=int(input())
-
-if(a>=18 and b>40):
-
-    print("True")
-
-else:
-
-    print("False")
+if(a<0):
+    a=a*-1
+print(a%10)
 
