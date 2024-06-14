25805103e24fba05dc747556576e0a58f67148b6
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 23:00:23 +0530

    Update Week-5.08.py

--- Week-5/Week-5.08.py
+++ Week-5/Week-5.08.py
@@ -1,112 +1,31 @@
-Merge Two Sorted Arrays Without Duplication
+Write a python program to count all letters, digits, and special symbols respectively from a given string
 
-Output is a merged array without duplicates.
 
-Input Format
+For example:
 
-N1 - no of elements in array 1
-
-Array elements for array 1
-
-N2 - no of elements in array 2
-
-Array elements for array2
-
-Output Format
-
-Display the merged array
+Input	Result
+rec@123
+3
+3
+1
 
 
 
-Sample Input 1
+a=input()
+ch=0
+num=0
+special=0
+for char in a:
+    if char.isalpha():
+        ch=ch+1
+    elif char.isdigit():
+        num=num+1
+    else:
+        special=special+1
+print(ch)
+print(num)
+print(special)
 
-
-
-5
-
-1 
-
-2 
-
-3 
-
-6 
-
-9
-
-4
-
-2 
-
-4 
-
-5 
-
-10
-
-
-
-Sample Output 1
-
-
-
-1 2 3 4 5 6 9 10
-
-
-
-def merge_arrays_without_duplicates(arr1, arr2):
-
-    # Combine the arrays and convert to a set to remove duplicates
-
-    result_set = set(arr1 + arr2)
-
-    # Convert the set back to a sorted list
-
-    merged_sorted_array = sorted(result_set)
-
-    return merged_sorted_array
-
- 
-
-# Input read and processing
-
-def process_input():
-
-    # Reading number of elements and the elements for the first array
-
-    n1 = int(input())
-
-    array1 = []
-
-    for _ in range(n1):
-
-        element = int(input())
-
-        array1.append(element)
-
- 
-
-    # Reading number of elements and the elements for the second array
-
-    n2 = int(input())
-
-    array2 = []
-
-    for _ in range(n2):
-
-        element = int(input())
-
-        array2.append(element)
-
- 
-
-    # Merge the arrays without duplicates
-
-    result = merge_arrays_without_duplicates(array1, array2)
-
- 
-
-    # Print the result
 
     print(" ".join(map(str, result)))
 
