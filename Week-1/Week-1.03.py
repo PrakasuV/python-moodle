37e4ef5ddb38e3d2d2bb1739bd096b06faee866e
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 16:32:51 +0530

    Update Week-1.03.py

--- Week-1/Week-1.03.py
+++ Week-1/Week-1.03.py
@@ -2,10 +2,6 @@ Square Root
 
 Write a simple python program to find the square root of a given floating point number. The output should be displayed with 3 decimal places.
 
-
-
-
-
 Sample Input:
 
 8.00
@@ -14,19 +10,20 @@ Sample Output:
 
 2.828
 
+
+
+
+
 For example:
 
 Input	Result
-
-14.00	3.742
-
-
+14.00
+3.742
 
 import math
-
 a=float(input())
-
-s=math.sqrt(a)
-
-print("{:.3f}".format(s))
-
+if math.sqrt(a)%2==0:
+    print(math.sqrt(a),end='')
+    print("00")
+else:
+    print(round(math.sqrt(a),3))
