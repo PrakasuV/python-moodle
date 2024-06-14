873888378a19d1f52b5b26c392b810274dcf78f0
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-14 22:59:00 +0530

    Update Week-5.06.py

--- Week-5/Week-5.06.py
+++ Week-5/Week-5.06.py
@@ -80,61 +80,54 @@ Input	Result
 
 1
 
-1	1
+1	1Consider the below words as key words and check the given input is key word or not.
+
+keywords: {break, case, continue, default, defer, else, for, func, goto, if, map, range, return, struct, type, var}
+
+Input format:
+
+Take string as an input from stdin.
+
+Output format:
+
+Print the word is key word or not.
+
+Example Input:
+
+break
+
+Output:
+
+break is a keyword
+
+Example Input:
+
+IF
+
+Output:
+
+IF is not a keyword
+
+
+For example:
+
+Input	Result
+break
+break is a keyword
+IF
+IF is not a keyword
 
 
 
-import sys
 
-import math
-
- 
-
-def find_factors(n):
-
-	factors = []
-
-	for i in range(1, int(math.sqrt(n)) + 1):
-
-    	if n % i == 0:
-
-        	factors.append(i)
-
-        	if i != n // i:
-
-            	factors.append(n // i)
-
-	return sorted(factors)
-
- 
-
-def get_pth_factor(n, p):
-
-	factors = find_factors(n)
-
-	if p <= len(factors):
-
-    	return factors[p - 1]
-
-	else:
-
-    	return 0
-
- 
-
-# Reading input directly from the standard input (typically for competitive programming)
-
-input = sys.stdin.read
-
-data = input().split()
-
-n = int(data[0])
-
-p = int(data[1])
-
- 
-
-# Calculate and print the p-th factor
-
-print(get_pth_factor(n, p))
-
+keywords=['break','case','continue','default','defer','else','for','func','goto','if','map','range','return','struct','type','var']
+s=input()
+flag=0
+for i in keywords:
+    if(s==i):
+        flag=1
+        break
+if(flag==1):
+    print(s,'is a keyword')
+else:
+    print(s,'is not a keyword')
