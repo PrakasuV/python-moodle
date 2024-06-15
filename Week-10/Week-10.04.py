0bc1f0be6f6a16b2d1dc55bf15e897154c61b99c
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:51:32 +0530

    Update Week-10.04.py

--- Week-10/Week-10.04.py
+++ Week-10/Week-10.04.py
@@ -1,27 +1,27 @@
-Binary Search
-
 Write a Python program for binary search.
-
-
 
 For example:
 
 Input	Result
-
-1 2 3 5 8
-
-6	False
-
-3 5 9 45 42
-
-42	True
+1,2,3,5,8
+6
+False
+3,5,9,45,42
+42
+True
 
 
 
 
 
-a = input().split(",")
 
-b = input()
-
-print(b in a)
+arr = list(map(int, input().split(',')))
+key = int(input())
+fg=0
+for i in range(len(arr)):
+    if arr[i] == key:
+        fg+=1
+if(fg):
+    print("True")
+else:
+    print("False")
