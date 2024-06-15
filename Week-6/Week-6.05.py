e5e2ec16f64ca3fff59e444f8070939cb0637885
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:32:16 +0530

    Update Week-6.05.py

--- Week-6/Week-6.05.py
+++ Week-6/Week-6.05.py
@@ -1,22 +1,73 @@
-Remove Palindrome Words
+Given an array A of sorted integers and another non negative integer k, find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j.
 
-String should contain only the words are not palindrome.
+Input Format
 
+1.      First line is number of test cases T. Following T lines contain:
 
+2.      N, followed by N integers of the array
 
-Sample Input 1
+3.      The non-negative integer k
 
-Malayalam is my mother tongue
+Output format
 
+Print 1 if such a pair exists and 0 if it doesn’t.
 
+Example
 
-Sample Output 1
+Input
 
-is my mother tongue
+1
 
+3 
+
+1 
+
+3 
+
+5
+
+4
+
+Output:
+
+1
+
+Input
+
+1
+
+3 
+
+1 
+
+3 
+
+5
+
+99
+
+Output
+
+0
 
 
 For example:
+
+Input	Result
+1
+3 
+1 
+3 
+5
+4
+1
+1
+3 
+1 
+3 
+5
+99
+0
 
 
 
@@ -24,17 +75,20 @@ For example:
 
 
 
-a=[]
-
-a=input()
-
-b=a. split()
-
-for i in b:
-
-    k=i.lower()
-
-    if k!=k[::-1]:
-
-         print(k,end=' ')
+a=int(input())
+for _ in range(a):
+    l=[]
+    s=0
+    n = int(input())
+    for _ in range(n):
+        l.append(int(input()))
+    k=int(input())
+    for i in range(n):
+        for j in range(i+1,n):
+            if l[j]-l[i]==k and i!=j:
+                s=1
+                break
+        if(s):
+            break
+    print(s)
 
