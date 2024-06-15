54a7c92c06f089de5ecd402a58983550c3651179
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:41:52 +0530

    Update Week-8.02.py

--- Week-8/Week-8.02.py
+++ Week-8/Week-8.02.py
@@ -1,68 +1,57 @@
-Check Pair
+In the game of Scrabble™, each letter has points associated with it. The total score of a word is the sum of the scores of its letters. More common letters are worth fewer points while less common letters are worth more points. The points associated with each letter are shown below:
 
-Given a tuple and a positive integer k, the task is to find the count of distinct pairs in the tuple whose sum is equal to K.
+Points Letters
 
-Examples:
+1 A, E, I, L, N, O, R, S, T and U
 
-Input: t = (5, 6, 5, 7, 7, 8 ), K = 13 
+2 D and G
 
-Output: 2 
+3 B, C, M and P
 
-Explanation: 
+4 F, H, V, W and Y
 
-Pairs with sum K( = 13) are  {(5, 8), (6, 7), (6, 7)}. 
+5 K
 
-Therefore, distinct pairs with sum K( = 13) are { (5, 8), (6, 7) }. 
+8 J and X
 
-Therefore, the required output is 2.
+10 Q and Z
+
+Write a program that computes and displays the Scrabble™ score for a word. Create a dictionary that maps from letters to point values. Then use the dictionary to compute the score.
+
+A Scrabble™ board includes some squares that multiply the value of a letter or the value of an entire word. We will ignore these squares in this exercise.
+
+Sample Input
+
+REC
+
+Sample Output
+
+REC is worth 5 points.
 
 
 
 For example:
 
 Input	Result
-
-1,2,1,2,5
-
-3	1
-
-1,2
-
-0	0
+REC
+REC is worth 5 points.
 
 
 
-t = input()
 
-k = int(input())
+letter_points = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1,
+    'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
+    'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1,
+    'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1,
+    'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4,
+    'Z': 10
+}
 
-a = t.split(",")
+word = input().upper()
 
-l = [int(x) for x in a]
+score = sum(letter_points[letter] for letter in word)
 
-count = 0
-
-x = set()
-
-
-
-for i in range(len(l)):
-
-    for j in range(i + 1, len(l)):
-
-        if l[i] + l[j] == k:
-
-            s = (l[i], l[j])
-
-            if s not in x and (l[j], l[i]) not in x:
-
-                count += 1
-
-                x.add(s)
-
-
-
-print(count)
-
+print(f"{word} is worth {score} points.")
 
 
