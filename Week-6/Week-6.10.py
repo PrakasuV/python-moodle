65126fffdcf0e2c4e4b778e0fd379d5640d788aa
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:35:16 +0530

    Update Week-6.10.py

--- Week-6/Week-6.10.py
+++ Week-6/Week-6.10.py
@@ -1,70 +1,149 @@
-Username Domain Extension
+Consider a program to insert an element / item in the sorted array. Complete the logic by filling up required code in editable section. Consider an array of size 10. The eleventh item is the data is to be inserted.
 
-Given a string S which is of the format USERNAME@DOMAIN.EXTENSION, the program must print the EXTENSION, DOMAIN, USERNAME in the reverse order.
+ 
+
+Sample Test Cases
+
+ 
+
+Test Case 1
+
+ 
+
+Input
+
+ 
+
+1
+
+3
+
+4
+
+5
+
+6
+
+7
+
+8
+
+9
+
+10
+
+11
+
+2
+
+ 
+
+Output
+
+ 
+
+ITEM to be inserted:2
+
+After insertion array is:
+
+1
+
+2
+
+3
+
+4
+
+5
+
+6
+
+7
+
+8
+
+9
+
+10
+
+11
+
+ 
+
+
+Test Case 2
+
+ 
+
+Input
+
+ 
+
+11
+
+22
+
+33
+
+55
+
+66
+
+77
+
+88
+
+99
+
+110
+
+120
+
+44
+
+ 
+
+Output
+
+ 
+
+ITEM to be inserted:44
+
+After insertion array is:
+
+11
+
+22
+
+33
+
+44
+
+55
+
+66
+
+77
+
+88
+
+99
+
+110
+
+120
 
 
 
-Input Format:
-
-The first line contains S.
 
 
-
-Output Format:
-
-The first line contains EXTENSION.
-
-The second line contains DOMAIN.
-
-The third line contains USERNAME.
-
-
-
-Boundary Condition:
-
-1 <= Length of S <= 100
-
-
-
-Example Input/Output 1:
-
-
-
-Input:
-
-
-
-vijayakumar.r@rajalakshmi.edu.in
-
-
-
-Output:
-
-
-
-edu.in
-
-rajalakshmi
-
-vijayakumar.r
-
-
-
-
-
-a = input()
-
-ext = a.split('@')[0]
-
-dom = a.split('@')[1].split('.')[0]
-
-userno = a.find('.')
-
-user = a[userno+1:]
-
-print(user)
-
-print(dom, end='\n')
-
-print(ext,end='\n')
+l=[]
+for i in range(10):
+    l.append(int(input()))
+c=int(input())
+l.append(c)
+l=sorted(l)
+print(f"ITEM to be inserted:{c}\nAfter insertion array is:")
+for i in l:
+    print(i)
 
