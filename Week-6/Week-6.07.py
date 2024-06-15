9a7b6493e4d67ad089277de81b3c7df835936832
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:33:49 +0530

    Update Week-6.07.py

--- Week-6/Week-6.07.py
+++ Week-6/Week-6.07.py
@@ -1,64 +1,109 @@
-Revers String
+Determine the factors of a number (i.e., all positive integer values that evenly divide into a number) and then return the pth element of the list, sorted ascending. If there is no pth element, return 0.
 
-Reverse a string without affecting special characters. Given a string S, containing special characters and all the alphabets, reverse the string without affecting the positions of the special characters.
+Example
 
+n = 20
 
+p = 3
 
-Input:
+The factors of 20 in ascending order are {1, 2, 4, 5, 10, 20}. Using 1-based indexing, if p = 3, then 4 is returned. If p > 6, 0 would be returned.
 
-A&B
+Constraints
 
-Output:
+1 ≤ n ≤ 1015
 
-B&A
+1 ≤ p ≤ 109
 
-Explanation: As we ignore '&' and
+The first line contains an integer n, the number to factor.
 
-As we ignore '&' and then reverse, so answer is "B&A".
+The second line contains an integer p, the 1-based index of the factor to return.
 
+Sample Case 0
 
+Sample Input 0
 
+10
+
+3
+
+Sample Output 0
+
+5
+
+Explanation 0
+
+Factoring n = 10 results in {1, 2, 5, 10}. Return the p = 3rd factor, 5, as the answer.
+
+Sample Case 1
+
+Sample Input 1
+
+10
+
+5
+
+Sample Output 1
+
+0
+
+Explanation 1
+
+Factoring n = 10 results in {1, 2, 5, 10}. There are only 4 factors and p = 5, therefore 0 is returned as the answer.
+
+Sample Case 2
+
+Sample Input 2
+
+1
+
+1
+
+Sample Output 2
+
+1
+
+Explanation 2
+
+Factoring n = 1 results in {1}. The p = 1st factor of 1 is returned as the answer.
 
 
 For example:
 
-
-
 Input	Result
-
-A&x#
-
-x&A#
-
-
-
-def reverse_string(s):
-
-    s = list(s)
-
-    l, r = 0, len(s) - 1
+10
+3
+5
+10
+5
+0
+1
+1
+1
 
 
 
-    while l < r:
+def factors_of_number(n):
+    factors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            factors.append(i)
+            if n // i != i:
+                factors.append(n // i)
+    factors.sort()
+    return factors
 
-        if not s[l].isalpha():
+def find_pth_factor(n, p):
+    factors = factors_of_number(n)
+    if p <= len(factors):
+        return factors[p - 1]
+    else:
+        return 0
 
-            l += 1
+# Input
+n = int(input())
+p = int(input())
 
-        elif not s[r].isalpha():
+# Output
+print(find_pth_factor(n, p))
 
-            r -= 1
-
-        else:
-
-            s[l], s[r] = s[r], s[l]
-
-            l += 1
-
-            r -= 1
-
-
-
-    return ''.join(s)
 
