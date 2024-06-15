75cc8855c394dda361315f99263e5776643844af
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:47:01 +0530

    Update Week-9.01.py

--- Week-9/Week-9.01.py
+++ Week-9/Week-9.01.py
@@ -1,75 +1,43 @@
-Uncommon words
+An e-commerce company plans to give their customers a special discount for Christmas.
 
-A sentence is a string of single-space separated words where each word consists only of lowercase letters.A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+They are planning to offer a flat discount. The discount value is calculated as the sum of all
 
-Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+the prime digits in the total bill amount.
 
-Example 1:
+Write an algorithm to find the discount value for the given total bill amount.
 
-Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+Constraints
 
-Output: ["sweet","sour"]
+1 <= orderValue< 10e100000
 
-Example 2:
+Input
 
+The input consists of an integer orderValue, representing the total bill amount.
 
+Output
 
-Input: s1 = "apple apple", s2 = "banana"
+Print an integer representing the discount value for the given total bill amount.
 
-Output: ["banana"]
+Example Input
 
- Constraints:
+578
 
-1 <= s1.length, s2.length <= 200
+Output
 
-s1 and s2 consist of lowercase English letters and spaces.
-
-s1 and s2 do not have leading or trailing spaces.
-
-All the words in s1 and s2 are separated by a single space.
-
-Note:
-
-Use dictionary to solve the problem
+12
 
 For example:
 
-Input	Result
-
-this apple is sweet
-
-this apple is sour
+Test	Result
+print(christmasDiscount(578))
+12
 
 
 
-	sweet sour
 
-
-
-a=input().split()
-
-b=input().split()
-
-c1,c2,l={},{},[]
-
-for i in a:
-
-    c1[i]=c1.get(i,0)+1
-
-for j in b:
-
-    c2[j]=c2.get(j,0)+1
-
-for w,c in c1.items():
-
-    if(c==1 and w not in b):
-
-        l.append(w)
-
-for w,c in c2.items():
-
-    if(c==1 and w not in a):
-
-        l.append(w)
-
-print(*l)
+def christmasDiscount(n):
+    discount = 0
+    for digit in str(n):
+        if int(digit) in [2, 3, 5, 7]:
+         discount += int(digit)
+    return discount  
