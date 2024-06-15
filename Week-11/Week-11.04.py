5994bd2b99f3fd8f9e6c64b7dff8f4db382d1f83
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:54:49 +0530

    Update Week-11.04.py

--- Week-11/Week-11.04.py
+++ Week-11/Week-11.04.py
@@ -1,60 +1,44 @@
-Develop a Python program that safely performs division between two numbers provided by the user. Handle exceptions like division by zero and non-numeric inputs.
+Problem Description:
 
-Input Format: Two lines of input, each containing a number.
+Develop a Python program that safely calculates the square root of a number provided by the user. Handle exceptions for negative inputs and non-numeric inputs.
 
-Output Format: Print the result of the division or an error message if an exception occurs.
+Input Format:
 
+User inputs a number.
 
+Output Format:
 
-
+Print the square root of the number or an error message if an exception occurs.
 
 For example:
 
 Input	Result
-
-10
-
-2	5.0
-
-10
-
-0	Error: Cannot divide or modulo by zero.
+16
+The square root of 16.0 is 4.00
+-4
+Error: Cannot calculate the square root of a negative number.
+rec
+Error: could not convert string to float
 
 
 
-ten
 
-5
+import math
 
-	Error: Non-numeric input provided.
-
-
+def calculate_square_root(num):
+    try:
+        num = float(num)
+        if num < 0:
+            print("Error: Cannot calculate the square root of a negative number.")
+        else:
+            result = math.sqrt(num)
+            print("The square root of {} is {:.2f}".format(num, result))
+    except ValueError:
+        print("Error: could not convert string to float")
 
 def main():
+    num_input = input()
+    calculate_square_root(num_input)
 
-    try:
-
-        num1 = float(input())
-
-        num2 = float(input())
-
-       
-
-        division_result = num1 / num2
-
-        modulo_result = num1 % num2
-
-       
-
-        print(division_result)
-
-        
-
-    except ValueError:
-
-        print("Error: Non-numeric input provided.")
-
-    except ZeroDivisionError:
-
-        print("Error: Cannot divide or modulo by zero.")
-
+if __name__ == "__main__":
+    main()
