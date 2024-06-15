1669e5313a3cfc9097ef2d2b46abc5137845324c
PrakasuV <231001146@rajalakshmi.edu.in>
2024-06-15 14:34:18 +0530

    Update Week-6.08.py

--- Week-6/Week-6.08.py
+++ Week-6/Week-6.08.py
@@ -1,36 +1,88 @@
-String characters balance Test
+Program to print all the distinct elements in an array. Distinct elements are nothing but the unique (non-duplicate) elements present in the given array.
 
+Input Format:
 
+First line take an Integer input from stdin which is array length n.
 
-Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter. If balanced display as "true" ,otherwise "false".
+Second line take n Integers which is inputs of array.
 
+Output Format:
+
+Print the Distinct Elements in Array in single line which is space Separated
+
+Example Input:
+
+5
+
+1 
+
+2 
+
+2 
+
+3 
+
+4
+
+Output:
+
+1 2 3 4
+
+Example Input:
+
+6
+
+1 
+
+1 
+
+2 
+
+2 
+
+3 
+
+3
+
+Output:
+
+1 2 3
 
 
 For example:
 
-
-
 Input	Result
+5
+1 
+2 
+2 
+3
+4
+1 2 3 4 
+6
+1 
+1 
+2 
+2 
+3 
+3
+1 2 3 
 
-Yn
-
-PYnative
-
-True
 
 
+def find_distinct_elements(arr):
+    distinct_elements = set(arr)
+    return distinct_elements
 
-a=input()
+# Input
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
 
-b=input()
-
-if a in b:
-
-    print("True")
-
-else:
-
-    print("False")
+# Output
+distinct_elements = find_distinct_elements(arr)
+print(*distinct_elements)
 
 
 
